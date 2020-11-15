646873dc2be23e89ec1252c986ef4ee4a1e93c3c
Denis Zhbankov <denis.zhbankov@gmail.com>
2020-11-15 15:33:14 +0300

    * feature/proof-of-concept: simplify start

--- scripts/start.js
+++ scripts/start.js
@@ -3,12 +3,7 @@ const { Moex } = require('../build')
 const moex = new Moex()
 
 ;(async function () {
-  const engines = await moex.getEngines()
-  // console.log('engines', engines)
-
-  const securities = await moex.getSecurities()
-  // console.log('securities', securities)
-
-  const security = await moex.getSecuritiesSecurity({ security: 'IMOEX' })
-  // console.log('security', security)
+  console.log(await moex.getEngines())
+  console.log(await moex.getSecurities())
+  console.log(await moex.getSecuritiesSecurity({ security: 'IMOEX' }))
 })()
